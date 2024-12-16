795288c7e5fbdffc88aa545a189583a99075b542
cmorales <caitlyn.morales424@gmail.com>
2024-12-16 13:11:12 -0500

    starting

---
+++ src/main/java/BatChars.java
@@ -0,0 +1,20 @@
+public class BatChars {
+
+    public String name;  
+    public String gimic; 
+    public boolean hasCostume; 
+    
+    
+}
+
+enum Alignment {
+    lawfulGood, 
+    neutralGood,
+    chaoticGood,
+    lawfulNeutral,
+    trueNeutral,
+    chaoticNeutral,
+    lawfulEvil,
+    neutralEvil,
+    chaoticEvil
+}    
---
+++ src/main/java/Villains.java
@@ -0,0 +1,3 @@
+public class Villains extends BatChars{
+    
+}
