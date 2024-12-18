4d89bb839279bfbc92e8bef05205e2d0bbe1b71c
cmorales <caitlyn.morales424@gmail.com>
2024-12-18 13:10:46 -0500

    classes

--- src/main/java/BatChars.java
+++ src/main/java/BatChars.java
@@ -1,10 +1,19 @@
 public class BatChars {
-
     public String name;  
+    public String alias;
     public String gimic; 
-    public boolean hasCostume; 
-    
-    
+    public boolean hasCostume;
+
+    public BatChars(String name, String alias, String gimic) {
+        this.name = name; 
+        this.alias = alias; 
+        this.gimic = gimic; 
+        this.hasCostume = true; 
+    }
+
+    public void noCostume() {
+        this.hasCostume = false; 
+    }
 }
 
 enum Alignment {
--- src/main/java/Vigilantes.java
+++ src/main/java/Vigilantes.java
@@ -1,3 +1,10 @@
 public class Vigilantes extends BatChars{
+    public int sidekicks; 
     
+
+    public Vigilantes(String name, String alias, String gimic, int sidekicks) {
+        super(name, alias, gimic); 
+        this.sidekicks = sidekicks; 
+
+    }
 }
